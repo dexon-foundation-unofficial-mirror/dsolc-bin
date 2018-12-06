4558230c32464701532fd5a4e94ad639f3209bc0
Travis CI <dev@dexon.org>
2018-12-06 08:25:41 +0000

    Added compiler version v0.5.1+commit.d7688b80

--- bin/list.js
+++ bin/list.js
@@ -1,11 +1,13 @@
 
 var soljsonSources = [
+  "soljson-v0.5.1+commit.d7688b80.js",
   "soljson-v0.5.1-nightly.2018.11.21+commit.d7688b80.js",
   "soljson-v0.5.0+commit.366fa976.js",
   "soljson-v0.5.0-nightly.2018.11.21+commit.366fa976.js",
   "soljson-v0.4.25+commit.433aebb7.js"
 ];
 var soljsonReleases = {
+  "0.5.1": "soljson-v0.5.1+commit.d7688b80.js",
   "0.5.0": "soljson-v0.5.0+commit.366fa976.js",
   "0.4.25": "soljson-v0.4.25+commit.433aebb7.js"
 };
